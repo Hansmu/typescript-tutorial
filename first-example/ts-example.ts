06f78c7e86a743995f99c1199b3a068c2d0fd19c
hansmu <hans.miikael@gmail.com>
2020-05-16 21:59:11 +0300

    Add example of type casting and optional chaining

--- first-example/ts-example.ts
+++ first-example/ts-example.ts
@@ -1,11 +1,18 @@
 const button = document.querySelector("button");
+const classButton = document.querySelector(".button");
 const input1 = document.getElementById("num1")! as HTMLInputElement; // ! can be used to tell TypeScript that it will never be null. It will always find an element. as does type casting.
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+const input2 = <HTMLInputElement>document.getElementById("num2")!; // Not able to use JSX? Thnk that as is the only option there.
 
-function add(num1: number, num2: number) {
+function addNums(num1: number, num2: number) {
     return num1 + num2;
 }
 
-button.addEventListener("click", function() {
-    console.log(add(+input1.value, +input2.value));
+button?.addEventListener("click", function() { // The optional property can be used for optional values. It can be used for chaining. potato?.variety?.color
+    console.log(addNums(+input1.value, +input2.value));
 });
+
+if (classButton) {
+    (classButton as HTMLInputElement).addEventListener('click', (e) => {
+        console.log(e);
+    });
+}
